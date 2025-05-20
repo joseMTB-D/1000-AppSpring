d6ac112ec5b69093a2f40e75a8ca49b0ebbde151
torbravo.dev <tobravo.dev@gmail.com>
2025-05-20 19:30:06 +0200

    20052025TS
    Se ha cambiado la web donde obtienen los datos a Europeana, dado que la web spain.info no facilitaba la obtencion de datos (de una forma legal)
    
    Añadida la clase recolectora de datos de la api Europeana.

--- 1001 - APITuristSites/1001/src/main/java/_1/TS/TsApplication.java
+++ 1001 - APITuristSites/1001/src/main/java/_1/TS/TsApplication.java
@@ -3,11 +3,22 @@ package _1.TS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import _1.TS.Europeana.RecolectarDatos;
+
 @SpringBootApplication
 public class TsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TsApplication.class, args);
+		testConexion();
+	}
+	private static void testConexion() {
+		RecolectarDatos recolectarDatos = new RecolectarDatos();
+		String endpoint = "https://api.europeana.eu/set/search?wskey=tleilcultb&query=creator%3A1eb0457d-9cc1-421e-adaf-ac5d9d4d5ab9+type%3ABookmarkFolder&profile=items";
+		String resultado = recolectarDatos.obtenerDatosPerfilEuropeana(endpoint);
+		System.out.println("Conexión a la API de Europeana exitosa.");
+		System.out.println("Datos obtenidos: " + resultado);
+		System.out.println("Fin de la prueba de conexión.");
 	}
 
 }
